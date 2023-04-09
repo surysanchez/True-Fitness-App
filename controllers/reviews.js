9aa3254bed7afb5ba55b033941111af0cd833438
surysanchez <surysanchezsales@gmail.com>
2023-04-08 22:18:20 -0400

    add create reviews functionality

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,8 +1,9 @@
 const Recipe = require('../models/recipe');
 
 module.exports = {
-    create
-}
+    create,
+    delete: deleteReview
+};
 
 async function create(req, res) {
     const recipe = await Recipe.findById(req.params.id);
@@ -12,6 +13,14 @@ async function create(req, res) {
     } catch (err) {
         console.log(err);
     }
+    res.redirect(`/recipes/${recipe._id}`);
+}
+
+async function deleteReview(req, res){
+    const recipe = await Recipe.findOne({'reviews._id': req.params.id, 'reviews.user': req.user._id });
+    
+    if(!recipe) return res.redirect('/recipes');
+    await recipe.save();
     res.redirect(`/recipes/${recipe._id}`);
 
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -6,5 +6,7 @@ const reviewsCtrl = require('../controllers/reviews');
 // POST /recipe/:id/reviews
 router.post('/recipes/:id/reviews', reviewsCtrl.create);
 
+// DELETE /reviews/:id
+router.delete('/reviews/:id', reviewsCtrl.delete)
 
 module.exports = router;
